c7517befeaa7383e487118d596b407fc139bda4d
Tykky <40919314+Tykky@users.noreply.github.com>
2020-01-29 04:08:44 +0000

    Add more tests for vector3D class

--- Test/vector3Dtest.cpp
+++ Test/vector3Dtest.cpp
@@ -1,31 +1,93 @@
 #include "gtest/gtest.h"
 #include "vector3D.h"
+#include <random>
 
-class vector3DSmallTest : public::testing::Test {
+using namespace std;
+
+class vector3Dtest : public::testing::Test {
 protected:
 
-    vector3D *v1;
-    vector3D *v2;
+    vector3D v1;
+    vector3D v2;
+    vector3D vr1;
+    vector3D vr2;
+
+    default_random_engine gen;
+    uniform_real_distribution<float> dist;
+
+    float x1, y1, z1;
+    float x2, y2, z2;
 
     virtual void SetUp() {
-        v1 = new vector3D(1,2,3);
-        v2 = new vector3D(3,4,5);
+
+        v1 = vector3D(1,2,3);
+        v2 = vector3D(3,4,5);
+
+        gen = default_random_engine();
+        dist = uniform_real_distribution<float>(-1000000,1000000);
+        auto roll = bind(dist,gen);
+
+        x1 = roll(); y1 = roll(); z1 = roll();
+        x2 = roll(); y2 = roll(); z2 = roll();
+
+        vr1 = vector3D(x1, y1, z1);
+        vr2 = vector3D(x2, y2, z2);
     }
-    virtual void TearDown() {
-        delete v1;
-        delete v2;
-    }
+
 };
-TEST_F(vector3DSmallTest, opPlusTest1) {
-    +(*v1);
-    ASSERT_EQ(1,v1->getX());
-    ASSERT_EQ(2, v1->getY());
-    ASSERT_EQ(3,v1->getZ());
+/**Tests operator +() */
+TEST_F(vector3Dtest, opPlusTestSmall) {
+    +(v1);
+    EXPECT_EQ(1,v1.getX());
+    EXPECT_EQ(2, v1.getY());
+    EXPECT_EQ(3,v1.getZ());
+}
+TEST_F(vector3Dtest, opPlusTestRandom) {
+    +(vr1);
+    EXPECT_EQ(x1,vr1.getX());
+    EXPECT_EQ(y1,vr1.getY());
+    EXPECT_EQ(z1, vr1.getZ());
 }
 
-TEST_F(vector3DSmallTest, opPlustTest2) {
-    (*v1) += (*v2);
-    ASSERT_EQ(4,v1->getX());
-    ASSERT_EQ(6,v1->getY());
-    ASSERT_EQ(8,v1->getZ());
+/** Tests operator += vector */
+TEST_F(vector3Dtest, opAddtTestSmall1) {
+    v1 += v2;
+    EXPECT_EQ(4,v1.getX());
+    EXPECT_EQ(6,v1.getY());
+    EXPECT_EQ(8,v1.getZ());
+}
+TEST_F(vector3Dtest, opAddTestRandom1) {
+    vr1 += vr2;
+    EXPECT_EQ(x1+x2, vr1.getX());
+    EXPECT_EQ(y1+y2, vr1.getY());
+    EXPECT_EQ(z1+z2, vr1.getZ());
+}
+
+/** Tests operator += constant */
+TEST_F(vector3Dtest, opAddTestSmall2) {
+    float c = 9;
+    v1 += c;
+    EXPECT_EQ(1+c,v1.getX());
+    EXPECT_EQ(2+c,v1.getY());
+    EXPECT_EQ(3+c,v1.getZ());
+}
+TEST_F(vector3Dtest, opAddTestRandom2) {
+    float c = dist(gen);
+    vr1 += c;
+    EXPECT_EQ(x1+c,vr1.getX());
+    EXPECT_EQ(y1+c,vr1.getY());
+    EXPECT_EQ(z1+c,vr1.getZ());
+}
+/** Tests operator -() */
+TEST_F(vector3Dtest, opMinusTestSmall) {
+    vector3D negv1 = -v1;
+    EXPECT_EQ(-1, negv1.getX());
+    EXPECT_EQ(-2, negv1.getY());
+    EXPECT_EQ(-3, negv1.getZ());
+}
+TEST_F(vector3Dtest, opMinusTestRandom) {
+    vector3D negvr1 = -vr1;
+    EXPECT_EQ(-x1, negvr1.getX());
+    EXPECT_EQ(-y1, negvr1.getY());
+    EXPECT_EQ(-z1, negvr1.getZ());
 }
