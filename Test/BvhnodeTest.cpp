6a83a3c0249b46259ec23601720cae55d704dab3
Tykky <40919314+Tykky@users.noreply.github.com>
2020-03-07 05:19:29 +0000

    Add tests for primitivelist and bvh

--- Test/BvhnodeTest.cpp
+++ Test/BvhnodeTest.cpp
@@ -1,69 +1,79 @@
 #include <primitives/Sphere.h>
 #include <materials/Lambertian.h>
+#include <primitives/Bvhnode.h>
 #include "gtest/gtest.h"
-#include "primitives/Primitivelist.h";
+#include <random>
 
+using namespace std;
 
-class PrimitivelistTest : public ::testing::Test {
+class BvhnodeTest : public ::testing::Test {
 protected:
 
-    Primitive *list[3];
+    Primitive *list[4];
     Material *mat;
-    Primitivelist *plist;
+    Bvhnode *bvh;
+    mt19937 engine;
+    uniform_real_distribution<float> dist;
+    function<float()> randomfloat;
 
     void SetUp() {
+        engine = mt19937(1337);
+        dist = uniform_real_distribution<float>(0, 1);
+        randomfloat = bind(dist, engine);
         mat = new Lambertian(Vector3D(1,1,1));
         list[0] = new Sphere(Vector3D(0,0,0),0.5,mat);
         list[1] = new Sphere(Vector3D(-1,0,0),0.5,mat);
         list[2] = new Sphere(Vector3D(1,0,0),0.5,mat);
-        plist = new Primitivelist(list,3);
+        list[3] = new Sphere(Vector3D(0,2,0),0.5,mat);
+        bvh = new Bvhnode(list,3,0,1,randomfloat);
     }
 
     void TearDown() {
         for (int i = 0; i < 3; ++i) {
             delete list[i];
         }
-        delete plist;
+        delete bvh;
+        delete mat;
     }
 };
 
-TEST_F(PrimitivelistTest, hitMiddleTest) {
+TEST_F(BvhnodeTest, hitMiddleTest) {
 
     Ray r(Vector3D(0,0,-2),Vector3D(0,0,-1));
     hitrecord record;
 
-    bool hit = plist->hit(r,-9999,99999,record);
+    bool hit = bvh->hit(r,-9999,99999,record);
 
     EXPECT_FLOAT_EQ(true, hit);
     EXPECT_FLOAT_EQ(0.5, record.p.getZ());
 
 }
 
-TEST_F(PrimitivelistTest, hitLeftTest) {
+TEST_F(BvhnodeTest, hitLeftTest) {
     Ray r(Vector3D(-1,0,-2),Vector3D(-1,0,-1));
     hitrecord record;
 
-    bool hit = plist->hit(r,-9999,99999,record);
+    bool hit = bvh->hit(r,-9999,99999,record);
 
     EXPECT_FLOAT_EQ(true,hit);
     EXPECT_FLOAT_EQ(0.3535533,record.p.getZ());
 }
 
-TEST_F(PrimitivelistTest, hitLeftRight) {
+TEST_F(BvhnodeTest, hitLeftRight) {
     Ray r(Vector3D(1,0,-2),Vector3D(1,0,-1));
     hitrecord record;
 
-    bool hit = plist->hit(r,-9999,99999,record);
+    bool hit = bvh->hit(r,-9999,99999,record);
 
     EXPECT_FLOAT_EQ(true,hit);
     EXPECT_FLOAT_EQ(0.3535533,record.p.getZ());
 }
 
-TEST_F(PrimitivelistTest, missTest) {
+TEST_F(BvhnodeTest, missTest) {
     Ray r(Vector3D(5,0,-2),Vector3D(5,0,-1));
     hitrecord record;
 
-    bool hit = plist->hit(r,-9999,99999,record);
+    bool hit = bvh->hit(r,-9999,99999,record);
     EXPECT_EQ(false,hit);
     EXPECT_EQ(0,record.p.getZ());
 }
