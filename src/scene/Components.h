2404db8aee8c01ff4664a921132f490613da3419
Tykky <40919314+Tykky@users.noreply.github.com>
2022-03-16 18:50:30 +0200

    Add fmt and more scene stuff

--- src/scene/Components.h
+++ src/scene/Components.h
@@ -1,7 +1,6 @@
 #ifndef RAYTRACER_COMPONENTS_H
 #define RAYTRACER_COMPONENTS_H
 
-#include <entt/entt.hpp>
 #include <glm/glm.hpp>
 #include "glm/gtc/matrix_transform.hpp"
 
@@ -16,12 +15,12 @@ struct TransformComponent
 
 	glm::mat4 getTransform()
 	{
-		glm::mat4 rot = glm::rotate(glm::mat4(1.0f), Rotation.x, { 1, 0, 0 }) *
-						glm::rotate(glm::mat4(1.0f), Rotation.y, { 0, 1, 0 }) *
-						glm::rotate(glm::mat4(1.0f), Rotation.z, { 0, 0, 1 });
+		glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), Rotation.x, { 1, 0, 0 }) *
+						     glm::rotate(glm::mat4(1.0f), Rotation.y, { 0, 1, 0 }) *
+						     glm::rotate(glm::mat4(1.0f), Rotation.z, { 0, 0, 1 });
 
-		return glm::translate(glm::mat4(1.0f), Translation) *
-			   rot *
+		return rotation * 
+			   glm::translate(glm::mat4(1.0f), Translation) *
 			   glm::scale(glm::mat4(1.0f), Scale);
 			   
 	}
--- src/scene/Scene.h
+++ src/scene/Scene.h
@@ -1,5 +1,13 @@
 #ifndef RAYTRACER_SCENE_H 
 #define RAYTRACER_SCENE_H
 
+ #include <entt/entt.hpp>
+
+class Scene
+{
+public:
+	Scene();
+private:
+};
 
 #endif // RAYTRACER_SCENE_H
